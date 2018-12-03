05b71616e8d75855bd046aba6c1ca737dc6593db
Michel Varela <roxguel@gmail.com>
2018-12-03 20:20:09 -0300

    Create TaskItem Component

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,4 +1,3 @@
-import styles from './TaskItem.css';
 import { Card } from 'antd';
 import { Component } from "react";
 
@@ -8,10 +7,18 @@ class TaskItem extends Component {
     constructor(props) {
         super(props);
         this.task = props.task;
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick() {
+        this.props.onClick(this.task);
     }
     render() {
         return (
-            <Card hoverable style={{marginBottom: 10}} cover={this.task.image ? <img alt={this.task.name} src={this.task.image} /> : null}>
+            <Card hoverable
+                onClick={this.handleClick}
+                style={{ margin: 10, marginBottom: 10, width: 'calc(25% - 20px)' }}
+                cover={this.task.image ? <img alt={this.task.name} src={this.task.image} /> : null}
+            >
                 <Meta title={this.task.name} description={this.task.description} />
             </Card>
         );
