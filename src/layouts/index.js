5c806ee3dfd01f5df3caf56ebcc814247378b110
Michel Varela <roxguel@gmail.com>
2018-12-05 00:19:41 -0300

    Remove menu

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Layout, Menu, Breadcrumb } from 'antd';
+import { Layout, Menu } from 'antd';
 import styles from './index.css';
 
 const { Header, Content, Footer } = Layout;
@@ -9,28 +9,22 @@ class BasicLayout extends Component {
 		super(props);
 		this.props = props;
 	}
+	
 	componentDidMount() {
-		// console.log(this.props.children.props);
+		
 	}
+
+	newTask = () => {
+		console.log('Mostrar formulario para nueva tarea');
+	}
+
 	render() {
 		return (
 			<Layout className="layout">
 				<Header>
-					<div className={styles.logo} />
-					<Menu
-						theme="dark"
-						mode="horizontal"
-						selectable={false}
-						defaultSelectedKeys={['1']}
-						style={{ lineHeight: '64px' }}>
-						<Menu.Item key="1">Tasks</Menu.Item>
-						<Menu.Item>New Task</Menu.Item>
-					</Menu>
+					<div className={styles.logo}>Tasks</div>
 				</Header>
-				<Content style={{ padding: '0 50px' }}>
-					<Breadcrumb style={{ margin: '16px 0' }}>
-						<Breadcrumb.Item>Tasks</Breadcrumb.Item>
-					</Breadcrumb>
+				<Content style={{ padding: '25px 50px' }}>
 					<div className={styles.content}>{this.props.children}</div>
 				</Content>
 				<Footer style={{ textAlign: 'center' }}>
